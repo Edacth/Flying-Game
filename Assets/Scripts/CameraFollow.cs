3fa70310e8f924c6656ea805819909fd705ded43
Marcel Dao <marcelnamdao@gmail.com>
2019-03-07 12:51:59 -0800

    implement basic camera following player

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour {
-
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform LeadObject;
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        LeadObject = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        transform.position = new Vector3(LeadObject.position.x, LeadObject.position.y, LeadObject.position.z - 20) / 2;
 	}
 }
