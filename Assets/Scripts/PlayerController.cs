d261188487f72612dc1da9ebcad1891d232189cb
jday151r <j.day151r@gmail.com>
2019-03-08 10:49:11 -0800

    Adds calibration (wip)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public Rigidbody rBody;
     public Transform gameAnchor;
     public Vector2 axisInput;
+    public Vector3 defaultPos;
     public float moveSpeed;
     public float xBoundary;
     public float yBoundary;
@@ -17,9 +18,13 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        rBody.AddForce(new Vector3(Input.gyro.gravity.x + Input.gyro.gravity.z, -Input.gyro.gravity.y, 0).normalized * moveSpeed);
+        rBody.AddForce(new Vector3(defaultPos.x - Input.gyro.gravity.x, defaultPos.y + Input.gyro.gravity.y, 0).normalized * moveSpeed);
         rBody.rotation = Input.gyro.attitude;
         rBody.AddForce(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * moveSpeed);
         transform.position = new Vector3(Mathf.Clamp(transform.position.x, -xBoundary, xBoundary), Mathf.Clamp(transform.position.y, -yBoundary, yBoundary), transform.position.z);
+    }
+    public void SetCalibration()
+    {
+        defaultPos = Input.gyro.gravity;
     }
 }
