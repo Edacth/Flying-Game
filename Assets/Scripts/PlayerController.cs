02269c5eacd1e9bd85707cfe5fd405e1ccf50f37
Marcel Dao <marcelnamdao@gmail.com>
2019-03-08 10:19:15 -0800

    wip gyro rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,12 +8,13 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     void Start()
     {
-
+        Input.gyro.enabled = true;
     }
 
     void FixedUpdate()
     {
-        rBody.AddForce(new Vector3(Input.acceleration.x, Input.acceleration.y, 0) * moveSpeed);
+        rBody.AddForce(new Vector3(Input.gyro.gravity.x, -Input.gyro.gravity.y, 0) * moveSpeed);
+        rBody.rotation = Input.gyro.attitude;
         rBody.AddForce(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * moveSpeed);
     }
 }
