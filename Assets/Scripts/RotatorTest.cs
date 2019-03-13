88df2e7ac7fc960a3e38e7cf69d2b1e8533fd38b
jday151r <j.day151r@gmail.com>
2019-03-13 13:37:03 -0700

    Adds rotator test scene

---
+++ Assets/Scripts/RotatorTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatorTest : MonoBehaviour
+{
+    public TextMesh gyroText;
+    public GameObject arrow;
+    // Use this for initialization
+    void Start()
+    {
+        Input.gyro.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gyroText.text = Input.gyro.attitude.eulerAngles.ToString();
+        arrow.transform.rotation = Input.gyro.attitude;
+    }
+}
