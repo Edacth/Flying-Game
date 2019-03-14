4297a5f7fcfcdfaf01fe9949d1ef42946179e2c3
jday151r <j.day151r@gmail.com>
2019-03-13 17:55:12 -0700

    Added color changing UI

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public int damage;
+    public RawImage jetOutline;
+    public TextMeshProUGUI damageText;
+    Color UIColor;
+    public Color UIStartColor;
+    public Color UIEndColor;
+
+    void Update()
+    {
+        UIColor = Color.Lerp(UIStartColor, UIEndColor, (float)damage / 100);
+        damageText.text = "DAMAGE " + damage + "%";
+        damageText.color = jetOutline.color = UIColor;
+    }
+}
