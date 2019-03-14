9dd997cd1ae1e49e05994d7ffcf35ace060287bd
Marcel Dao <marcelnamdao@gmail.com>
2019-03-13 17:30:50 -0700

    sample font and shooting

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    float timer = 0;
+
+    void OnEnable()
+    {
+
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        timer += Time.deltaTime;
+        if (timer >= 1 && gameObject.activeSelf)
+        {
+            gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            gameObject.SetActive(false);
+            timer = 0;
+        }
+    }
+}
