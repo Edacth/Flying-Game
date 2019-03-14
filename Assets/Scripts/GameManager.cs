b5533eda07fd0ec1fc25da5d601540693dcf9d6b
Cade Anderson <cadeanderson99@gmail.com>
2019-03-13 17:51:18 -0700

    Create GameManager and expand environment generation

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null; //Instance of this script
+    public GameObject environmentSection; 
+    public int numOfSections;
+    List<ESectionController> ESectionPool = new List<ESectionController>();
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start () {
+        for (int i = 0; i < numOfSections; i++)
+        {
+            Vector3 pos = new Vector3(0, -5, 40 * i + 40);
+            GameObject section = Instantiate(environmentSection, pos, Quaternion.identity);
+            ESectionPool.Add(section.GetComponent<ESectionController>());
+
+            ESectionPool[i].GenerateTower();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            for (int i = 0; i < numOfSections; i++)
+            {
+                ESectionPool[i].GenerateTower();
+            }
+        }
+
+        for (int i = 0; i < numOfSections; i++)
+        {
+            if (ESectionPool[i].transform.position.z < -80)
+            {
+                ESectionPool[i].transform.position = new Vector3(0, -5, ESectionPool[i].transform.position.z + 40 * numOfSections);
+            }
+            
+        }
+    }
+}
