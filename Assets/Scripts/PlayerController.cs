014c3bd360f8b09fcf35276548623a6e3c6603a2
Cade Anderson <cadeanderson99@gmail.com>
2019-04-04 10:45:18 -0700

    Add a delay to the end screen
    Assign the target gameObject to the playerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -123,7 +123,8 @@ public class PlayerController : MonoBehaviour
             Instantiate(explosion, transform.position, Quaternion.identity);
             dead = true;
 
-            menuController.SetEndState(true);
+            menuController.StartCoroutine("EndScreenDelay");
+
             GM.Save();
         }
         else
