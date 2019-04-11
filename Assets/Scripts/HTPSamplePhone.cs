459f5a898082d3b24a35de20f39999b4d6f5f0cd
jday151r <j.day151r@gmail.com>
2019-04-10 17:26:07 -0700

    Main menu canvas anchors fixed

--- Assets/Scripts/HTPSamplePhone.cs
+++ Assets/Scripts/HTPSamplePhone.cs
@@ -6,14 +6,15 @@ public class HTPSamplePhone : MonoBehaviour
 {
     public float timer = 0;
     public bool axis;
+    public float angle;
 
     void Update()
     {
         timer += Time.deltaTime * 3;
         if(axis)
-            transform.eulerAngles = new Vector3(0, Mathf.Sin(timer) * 45, 0);
+            transform.eulerAngles = new Vector3(0, Mathf.Sin(timer) * angle, 0);
         else
-            transform.eulerAngles = new Vector3(Mathf.Sin(timer) * 45, 0, 0);
+            transform.eulerAngles = new Vector3(Mathf.Sin(timer) * angle, 0, 0);
         if (timer >= 2 * Mathf.PI)
         {
             timer = 0;
